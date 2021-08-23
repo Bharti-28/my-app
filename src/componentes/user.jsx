1e17f34eef67f4dfd11cbb0611c236668dbea46a
Bharti-28 <patilbharti959@gmail.com>
2021-08-23 17:36:34 +0530

    life cycle methods and hooks

--- src/componentes/user.jsx
+++ src/componentes/user.jsx
@@ -1,48 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-class User extends React.Component {
-    constructor(props) {
-        super(props);
+const User = (props) => {
+    const [planet, setPlanet] = useState("earth");
+    
+    //componentDidMount
+    useEffect(() => {
+        //heavy compition
+        console.log("component mounting");
 
-        this.state = {
-            planet: "earth",
-        };
-        console.log("Hey Im from Constructor");
-    }
+        //componentWillUnMount
+        return console.log("bye bye");
+    }, []);
+    
+    //componentDidUpdate
+    //shouldComponentUpdate
+    useEffect(() => {
+        console.log("Planet changes");
+    }, [planet]);
 
-    componentDidMount() {
-        this.setState({ planet: "Jupiter" });
-    }
-
-    shouldComponentUpdate(nextProp, nextState){
-        console.log("from shouldComponentUpdate");
-        console.log({ 
-            nextProp,
-            nextState,
-         });
-        return true;
-    }
-
-    getSnapshotBeforeUpdate(prevProp, prevState){
-        console.log("from getSnapshotBeforeUpdate");
-        console.log({ prevProp, prevState });
-        return true;
-    }
-
-    componentDidUpdate() {
-        console.log(this.state);
-    }
-
-    render() {
-        console.log("Hey Im from render() ");
-        return (
-            <div>
-                <h1> {this.props.name} </h1>
-                <p> {this.props.description}</p>
-                <h4> {this.state.planet} </h4>
-                </div>
-        );
-    }
-}
+    return (
+        <div>
+            <h1> {props.name} </h1>
+            <p> {props.description} </p>
+            <button onClick={() => setPlanet("pluto")}>{planet}</button>
+        </div>
+    );
+};
 
 export default User;
