15ae101f6858000ccebb5776dfc062ff1d63e91f
Bharti-28 <patilbharti959@gmail.com>
2021-08-23 16:00:16 +0530

    component life cycle method

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import User from "./componentes/user";
+
+
+function App() {
+  return (
+   <div>
+     <h1>Users</h1>
+     <User name="Pavan" description="FSWD" />
+  </div>
+  );
+}
+
+export default App;
