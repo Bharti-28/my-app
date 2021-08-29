eaaf08c40e0ccab9be2743c50338a6d131e04395
Bharti-28 <patilbharti959@gmail.com>
2021-08-29 16:23:11 +0530

    add entertainment cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import User from "./componentes/user";
+import { Route } from "react-router-dom";
 
+//HOC
+import DefaultHOC from "./HOC/Default.HOC";
+
+//Pages
+import HomePage from "./Pages/Home.page";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
-   <div>
-     <h1>Users</h1>
-     <User name="Pavan" description="FSWD" />
-  </div>
+    <>
+      <DefaultHOC path="/" exact component={HomePage} />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Pages/Home.page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+//components
+import EntertainmentCardSlider from "../components/Entertainment/EntertainmentCard.component";
+
+const HomePage = () => {
+    return (
+    <>
+      <EntertainmentCardSlider />
+    </>
+    );
+};
+
+export default HomePage;
